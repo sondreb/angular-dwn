9318a9a443de92691fe3d71a6874c263a21eec85
SondreB <sondre@outlook.com>
2022-10-20 16:15:57 +0200

    Add the dwn-sdk-js
    - This results in compilation errors due to "assert" on imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Dwn } from '@tbd54566975/dwn-sdk-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dwn-ui';
+
+  constructor() {
+    console.log(Dwn);
+  }
 }
